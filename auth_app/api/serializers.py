874d53b74aa10ec1fac4adad2a2cb3e97e836584
Maik G <maik.g@outlook.com>
2026-04-28 09:57:17 +0200

    Add: Split function for First and Lastname + username Regex Validation only letter and space

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator 
 
 class RegistrationSerializer(serializers.ModelSerializer):
 
     repeated_password = serializers.CharField(write_only=True)
+    username = serializers.CharField(
+        validators=[RegexValidator(r'^[a-zA-ZäöüÄÖÜß\s]+$', 'Only letters and spaces allowed.')]
+    )
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'repeated_password']
@@ -11,12 +15,20 @@ class RegistrationSerializer(serializers.ModelSerializer):
         'password': {'write_only': True},
         'email': {'required': True}
     }
+
+    def validate_username(self, value):
+        if len(value.split()) < 2:
+            raise serializers.ValidationError({'error': 'Enter your Firstname and Lastname'})
+        return value
+    
         
     def save(self):
         pw = self.validated_data['password']
         repeated_password = self.validated_data['repeated_password']
         email = self.validated_data['email']
         all_email = User.objects.values_list('email', flat=True)
+        full_name = self.validated_data['username']
+        name_parts = full_name.split()
 
         if pw != repeated_password:
             raise serializers.ValidationError({'error': 'password dont match'})
@@ -24,7 +36,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
         if email in all_email:
             raise serializers.ValidationError({'error': 'email is used'})
         
-        account = User(email = self.validated_data['email'], username=self.validated_data['username'])
+        account = User(email = self.validated_data['email'], username=self.validated_data['username'], first_name=name_parts[0], last_name=' '.join(name_parts[1:]) )
         account.set_password(pw)
         account.save()
         return account
