96c8bc9dfff7111d46193aca61e5c43d7c5fd0c4
Maik G <maik.g@outlook.com>
2026-04-28 08:18:15 +0200

    Add: implement user registration functionality with validation and token generation

---
+++ auth_app/api/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class RegistrationSerializer(serializers.ModelSerializer):
+
+    repeated_password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'repeated_password']
+        
+    def save(self):
+        pw = self.validated_data['password']
+        repeated_password = self.validated_data['repeated_password']
+        email = self.validated_data['email']
+        all_email = User.objects.values_list('email', flat=True)
+
+        if pw != repeated_password:
+            raise serializers.ValidationError({'error': 'password dont match'})
+        
+        if email in all_email:
+            raise serializers.ValidationError({'error': 'email is used'})
+        
+        account = User(email = self.validated_data['email'], username=self.validated_data['username'])
+        account.set_password(pw)
+        account.save()
+        return account
--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class UserRegestration(models.Model):
+    username = models.CharField(max_length= 120)
+    email = models.CharField(max_length= 120)
+    password = models.CharField(max_length=255)
+    
