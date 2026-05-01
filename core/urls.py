18392c9a92c56d452f88185098e304ed40b585f4
Maik G <maik.g@outlook.com>
2026-05-01 09:27:36 +0200

    Add: create board_app with initial structure including models, views, serializers

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('auth_app.api.urls')),
+    path('api/', include('board_app.api.urls')),
     path('api/', include('task_app.api.urls')),
 
     
