2c5403c77e7e67fbadb7efad2a347a6f29429541
Maik G <maik.g@outlook.com>
2026-05-03 19:28:29 +0200

    Add: implement post method in BoardListView for creating new boards; update BoardDetailView class indentation

--- board_app/api/urls.py
+++ board_app/api/urls.py
@@ -4,4 +4,5 @@ from .views import BoardListView, BoardDetailView
 urlpatterns = [
     path('boards/', BoardListView.as_view(), name='board-list'),
     path('boards/<int:pk>/', BoardDetailView.as_view(), name='board-detail'),
+    path('email-check/', BoardDetailView.as_view(), name='email-check'),
 ]
--- board_app/api/views.py
+++ board_app/api/views.py
@@ -14,6 +14,14 @@ class BoardListView(APIView):
             return Response(serializer.data, status=200)
         return Response(serializer.errors, status=400)
     
+    def post(self, request):
+
+        serializer = BoardSerializer(data=request.data)
+        if serializer.is_valid:
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    
 class BoardDetailView(APIView):
     permission_classes = [IsRieviewerOrAssigneeOrAdmin]
 
